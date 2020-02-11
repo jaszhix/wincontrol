e0a437f79baeb2fa047ae19a8af93073fd7df9e1
jason <jaszhix@gmail.com>
2020-02-10 20:00:00 -0600

    Add getAffinityForCoreRanges test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,6 @@
-
 import {parseProfilesConfig} from './index';
 
-import {readYamlFile} from './utils';
+import {getAffinityForCoreRanges, readYamlFile} from './utils';
 
 test('parseProfilesConfig: can parse profiles', async (done) => {
   const appConfig = await readYamlFile('./test/config.yaml');
@@ -21,4 +20,52 @@ test('parseProfilesConfig: can parse profiles', async (done) => {
   );
 
   done();
+});
+
+test('getAffinityForCoreRanges: converts 2D arrays of core ranges to Windows compatible affinity values', () => {
+  let [affinity, graph] = getAffinityForCoreRanges([[0, 13]], true, 28);
+
+  expect(affinity).toBe(268435455);
+  expect(graph).toBe('[ 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|13]');
+
+  [affinity, graph] = getAffinityForCoreRanges([[0, 12]], true, 28);
+
+  expect(affinity).toBe(134209535);
+  expect(graph).toBe('[ 0| 1| 2| 3| 4| 5| 6| 7| 8| 9|10|11|12|--]');
+
+  [affinity, graph] = getAffinityForCoreRanges([[0, 1], [11, 13]], true, 28);
+
+  expect(affinity).toBe(234944515);
+  expect(graph).toBe('[ 0| 1|--|--|--|--|--|--|--|--|--|11|12|13]');
+
+  [affinity, graph] = getAffinityForCoreRanges([[10]], true, 28);
+
+  expect(affinity).toBe(16778240);
+  expect(graph).toBe('[--|--|--|--|--|--|--|--|--|--|10|--|--|--]');
+
+  [affinity, graph] = getAffinityForCoreRanges([[0, 3], [6, 9], [12, 13]], true, 28);
+
+  expect(affinity).toBe(217314255);
+  expect(graph).toBe('[ 0| 1| 2| 3|--|--| 6| 7| 8| 9|--|--|12|13]');
+
+  // Logical core affinity
+  [affinity, graph] = getAffinityForCoreRanges([[0, 3], [6, 9], [12, 13]], false, 28);
+
+  expect(affinity).toBe(13263);
+  expect(graph).toBe('[ 0| 1| 2| 3|--|--| 6| 7| 8| 9|--|--|12|13|--|--|--|--|--|--|--|--|--|--|--|--|--|--]');
+
+  [affinity, graph] = getAffinityForCoreRanges([[0, 3]], false, 4);
+
+  expect(affinity).toBe(15);
+  expect(graph).toBe('[ 0| 1| 2| 3]');
+
+  [affinity, graph] = getAffinityForCoreRanges([[0, 3]], false, 8);
+
+  expect(affinity).toBe(15);
+  expect(graph).toBe('[ 0| 1| 2| 3|--|--|--|--]');
+
+  [affinity, graph] = getAffinityForCoreRanges([[0, 3]], true, 8);
+
+  expect(affinity).toBe(255);
+  expect(graph).toBe('[ 0| 1| 2| 3]');
 });
